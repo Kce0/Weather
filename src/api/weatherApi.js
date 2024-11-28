5b175add75ee9e281e64c781bd35ef64a4765401
Kce0 <ilovekce@naver.com>
2024-11-29 01:32:50 +0900

    날씨사이트

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// Base URL과 API 키
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
-const AUTH_KEY = "d628236a7152afcb4edad367e87a524a"; // 실제 API 키로 교체
+const AUTH_KEY = "d628236a7152afcb4edad367e87a524a";
 
 const weatherApi = axios.create({
   baseURL: BASE_URL,
@@ -11,18 +10,18 @@ const weatherApi = axios.create({
   },
 });
 
-//https://api.openweathermap.org/data/2.5/weather?q=incheon&appid=내API키&units=metric&lang=kr
-
+// 날씨 검색 함수
 export const searchWeather = async (query) => {
-  const response = await weatherApi.get("/weather", {
-    params: {
-      q: query,
-      appid: AUTH_KEY,
-      units: "metric",
-      lang: "kr",
-    },
-  });
-  return response;
+  const params = {
+    q: query,
+    appid: AUTH_KEY,
+    units: "metric",
+    lang: "kr",
+  };
+
+  // API 요청 보내기
+  const response = await weatherApi.get("/weather", { params });
+  return response.data;
 };
 
 export default weatherApi;
--- src/features/weather/weatherSlice.js
+++ src/features/weather/weatherSlice.js
@@ -4,26 +4,19 @@ import { searchWeather } from "../../api/weatherApi";
 // 날씨 검색을 위한 비동기 액션
 export const fetchSearchResults = createAsyncThunk(
   "weather/fetchSearchResults",
-  async (query, { rejectWithValue }) => {
-    try {
-      const response = await searchWeather(query); // weatherApi에서 반환된 데이터를 바로 반환
-      return response; // 성공시 data를 반환
-    } catch (error) {
-      // 에러 발생 시 메시지 반환
-      return rejectWithValue(error.message);
-    }
+  async ({ query }) => {
+    const response = await searchWeather(query);
+    return response;
   }
 );
 
-const initialState = {
-  loading: false,
-  error: null,
-  searchResults: {}, // 날씨 결과는 객체 형태로 저장
-};
-
 const weatherSlice = createSlice({
   name: "weather",
-  initialState,
+  initialState: {
+    loading: false,
+    error: null,
+    searchResults: {},
+  },
   reducers: {},
   extraReducers: (builder) => {
     builder
@@ -33,11 +26,11 @@ const weatherSlice = createSlice({
       })
       .addCase(fetchSearchResults.fulfilled, (state, action) => {
         state.loading = false;
-        state.searchResults = action.payload; // action.payload에 있는 날씨 데이터를 저장
+        state.searchResults = action.payload;
       })
       .addCase(fetchSearchResults.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload; // 에러 메시지 처리
+        state.error = action.error.message;
       });
   },
 });
