de7790545d838e4e5145f7ff58860fe3fd68045f
Kce0 <ilovekce@naver.com>
2024-12-18 17:26:16 +0900

    ?

--- src/pages/WeatherCategory.jsx
+++ src/pages/WeatherCategory.jsx
@@ -2,12 +2,8 @@ import "../styles/common.css";
 import { Wrap, Main } from "../styles/StyledComponent";
 import Menu from "../components/Menu";
 import Footer from "../components/Footer";
-import { useDispatch, useSelector } from "react-redux";
 
 function WeatherCategory({ category }) {
-  const dispath = useDispatch();
-  const { weathers, loading, error } = useSelector((state) => state.weathers);
-
   return (
     <Wrap>
       <Menu />
